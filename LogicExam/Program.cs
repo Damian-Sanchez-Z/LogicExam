a8a3266bc744fa6373c212275530f198d111f649
Damian Sanchez <dsanchezzarfino@frba.utn.edu.ar>
2022-06-30 20:09:06 -0300

    Add project files.

---
+++ LogicExam/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogicExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+
+            text = text.Replace(" ", "");
+            var textLength = text.Length;
+            var colRowLength = Math.Sqrt(textLength);
+            var array = new List<string>();
+            var i = 0;
+            var row = "";
+            while(text.Length > i)
+            {
+                if(colRowLength > row.Length)
+                {
+                    row += text[i];
+                }
+                else
+                {
+                    array.Add(row);
+                    row = text[i].ToString();
+                }
+                i++;
+            }
+            array.Add(row);
+            //Console.WriteLine(string.Join("\n",array));
+
+            var returnText = "";
+            for (int j = 0; j< Math.Ceiling(colRowLength); j++)
+            {
+                foreach (var item in array)
+                {
+                    if(item.Length > j)
+                    {
+                        returnText += item[j];
+                    }
+                }
+                returnText = j!= colRowLength-1 ? returnText + " ": returnText; 
+            }
+
+            Console.WriteLine(returnText);
+
+
+            Console.ReadLine();
+        }
+    }
+}
